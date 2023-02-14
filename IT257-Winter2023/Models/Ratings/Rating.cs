a699451e19e8532d992b80ca23f68b12984de53f
Brandon Green <bgreen025007@students.iticollege.edu>
2023-02-14 17:51:46 -0600

    Implementing Ratings System

---
+++ IT257-Winter2023/Models/Ratings/Rating.cs
@@ -0,0 +1,10 @@
+﻿namespace IT257_Winter2023.Models.Ratings
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int ProviderID { get; set; }
+        public RatingsProvider Provider { get; set; }
+        public string RatingValue { get; set; }
+    }
+}
---
+++ IT257-Winter2023/Models/Ratings/RatingsProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace IT257_Winter2023.Models.Ratings
+{
+    public class RatingsProvider
+    {
+        public int Id { get; set; }
+        public string ProviderName { get; set; }
+    }
+}
