fb177427730ef041e84e8475ef9f078a049f4489
Brandon Green <bmmgreen@gmail.com>
2023-02-13 23:50:04 -0600

    Iterating Genre and Console

--- IT257-Winter2023/Models/Console.cs
+++ IT257-Winter2023/Models/Console.cs
@@ -1,8 +1,11 @@
-﻿namespace IT257_Winter2023.Models
+﻿using System.Collections.Generic;
+
+namespace IT257_Winter2023.Models
 {
     public class ConsoleModel
     {
         public int ConsoleId { get; set; }
         public string ConsoleName { get; set; }
+        public IEnumerable<GameModel> Games { get; set; }
     }
 }
--- IT257-Winter2023/Models/Genre.cs
+++ IT257-Winter2023/Models/Genre.cs
@@ -1,8 +1,12 @@
-﻿namespace IT257_Winter2023.Models
+﻿using System.Collections.Generic;
+
+namespace IT257_Winter2023.Models
 {
     public class GenreModel
     {
         public int GenreId { get; set; }
         public string GenreName { get; set; }
+
+        public IEnumerable<GameModel> Games { get; set; }
     }
 }
