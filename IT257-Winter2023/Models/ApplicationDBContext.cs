92ff08a8f0357688a0907f8af1caa4a6527a714f
Brandon Green <bgreen025007@students.iticollege.edu>
2023-02-14 18:03:27 -0600

    Implementing Database Context

---
+++ IT257-Winter2023/Models/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using IT257_Winter2023.Models.Games;
+using IT257_Winter2023.Models.Ratings;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT257_Winter2023.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options) {
+
+        }
+        public DbSet<GenreModel> Genres { get; set; }
+        public DbSet<ConsoleModel> Consoles { get; set; }
+        public DbSet<RatingsProvider> RatingsProviders { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<GameModel> Games { get; set; }
+
+    }
+}
---
+++ IT257-Winter2023/Models/EntityFrameworkRepository.cs
@@ -0,0 +1,10 @@
+﻿using IT257_Winter2023.Models.Games;
+using System.Linq;
+
+namespace IT257_Winter2023.Models
+{
+    public interface IGameRepository
+    {
+        IQueryable<GameModel> Games { get; }
+    }
+}
