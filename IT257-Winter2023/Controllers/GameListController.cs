3f0803b643689219cb3c6f44828cc6c1133031fa
Chase Hays <chays024872@students.iticollege.edu>
2023-02-14 21:18:42 -0600

    Added GameList Controller

---
+++ IT257-Winter2023/Controllers/GameListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT257_Winter2023.Controllers
+{
+    public class GameListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
